8cd3af7cbde6047de2a57b67c65e7667173bf940
abdul awal suhail <abdullahsuhail43@gmail.com>
2022-05-22 11:08:58 +0600

    mongodb conected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config;
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -14,6 +15,13 @@ app.use(express.json());
 // aas_electronics
 // xQDszruTIvlC3IW7
 
+
+
+
+const uri = "mongodb+srv://aas_electronics:xQDszruTIvlC3IW7@cluster0.atw05.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+
 app.get('/', (req, res) => {
     res.send('Running Genius Server');
 });
@@ -23,5 +31,5 @@ app.get('/hero', (req, res) =>{
 })
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
+    console.log(`AAS Electronics App listening on port ${port}`)
 })
